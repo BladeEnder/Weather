a5d20f7b5ba8fb1b701a24c075c074edde948683
Ender <enderblade@protonmail.com>
2019-09-20 12:40:40 +0200

    initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>FORMULAIRE</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Form;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>TEMPERATURE</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Weather;
